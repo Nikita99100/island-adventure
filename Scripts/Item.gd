29280b4fa907cfdd51f3bf2320417e186f451cf6
Nikita99100 <nik-star99@mail.ru>
2021-07-18 22:41:49 +0300

    add ability to pick up items

--- Scripts/Item.gd
+++ Scripts/Item.gd
@@ -1,9 +1,32 @@
 extends RigidBody2D
 
 export var ID = -1
-onready var marsh = get_node("../Marshmallow")
+export var texture : Texture
+
+onready var player = get_node("../Marshmallow")
+
+var isCanPickedUp =  false
+var isPickedUp = false
 
 func _ready():
 	
-	pass # Replace with function body.
+	pass
 
+func _process(delta):
+	if isPickedUp:
+		self.global_position=lerp(self.global_position,player.global_position,delta*7)
+		if(global_position.distance_to(player.global_position)<3):
+			queue_free()
+	if Input.is_action_pressed("ui_pickup") and isCanPickedUp:
+		isPickedUp = true;
+	pass
+
+func _on_body_entered(body):
+	if body == player:
+		isCanPickedUp = true
+	pass
+
+func _on_body_exited(body):
+	if body == player:
+		isCanPickedUp = false
+	pass
